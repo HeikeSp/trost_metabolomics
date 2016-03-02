090bb713ecfa39050121283d920acaa51e236cc9
Heike Sprenger <HeikeSp@yahoo.com>
2016-03-02 10:40:17 +0100

    changed color values for crossing or lines

--- scripts/gmd_data_analysis_jki_shelter_2014.R
+++ scripts/gmd_data_analysis_jki_shelter_2014.R
@@ -34,7 +34,8 @@ do_agg_late_before_samples = 0
 names_func_boxplot_2fac <- names_treatment_sample_time
 
 # Colors for Plots
-cols_cultivar_factor <- cols_lines_2014
+cols_genotype_factor <- cols_crossing
+cols_genotype_name_factor <- cols_lines_2014
 cols_sample_time_factor <- cols_sample_time2
 
 # Sample names corresponding to RNASeq data
